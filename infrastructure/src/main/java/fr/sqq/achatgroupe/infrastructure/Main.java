aa06d9a9fa814818384a6d44f3ca66db6a34a6c2
Lucas Declercq <lucas.declercq@hoohoot.org>
2026-02-15 09:52:08 +0100

    feat: add main class

---
+++ infrastructure/src/main/java/fr/sqq/achatgroupe/infrastructure/Main.java
@@ -0,0 +1,21 @@
+package fr.sqq.achatgroupe.infrastructure;
+
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+@QuarkusMain
+public class Main {
+    public static void main(String... args) {
+        Quarkus.run(SqqAchatGroupe.class, args);
+    }
+
+    public static class SqqAchatGroupe implements QuarkusApplication {
+
+        @Override
+        public int run(String... args) throws Exception {
+            Quarkus.waitForExit();
+            return 0;
+        }
+    }
+}
